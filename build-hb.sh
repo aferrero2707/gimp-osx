1a3bb912cc1732b706fae2cf472b2f0e8faa92a1
aferrero2707 <aferrero1975@gmail.com>
2018-05-18 10:17:00 +0200

    Update build-hb.sh

--- build-hb.sh
+++ build-hb.sh
@@ -24,8 +24,8 @@ export CC=gcc-8
 export CXX=g++-8
 
 
-HOMEBREW_NO_AUTO_UPDATE=1 brew install --build-from-source exiv2
-HOMEBREW_NO_AUTO_UPDATE=1 brew install intltool gettext json-c json-glib glib-networking gexiv2
+HOMEBREW_NO_AUTO_UPDATE=1 brew install intltool gettext json-c json-glib glib-networking
+HOMEBREW_NO_AUTO_UPDATE=1 brew reinstall --build-from-source exiv2 gexiv2
 HOMEBREW_NO_AUTO_UPDATE=1 brew info json-glib glib glib-networking gexiv2
 #HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD babl
 #HOMEBREW_NO_AUTO_UPDATE=1 brew install --HEAD gegl
